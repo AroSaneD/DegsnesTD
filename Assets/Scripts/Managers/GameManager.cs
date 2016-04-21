513607f8f265a30af7b6628bcc50c24afe442ad8
Arūnas Seniucas <Arnas Seniucas>
2016-04-21 23:30:37 +0300

    Added a basic unit moving from start to end

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using Assets.Scripts.Movement;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        //Todo: Make this class globally public and its instance reachable (singleton?)
+
+        public GameObject StartPointPrefab;
+        public GameObject EndPointPrefab;
+
+        private StartPosition _startingPointObject;
+        private EndPosition _endPointObject;
+
+        //Mob types
+        public Object BaseMob;
+        //private Object BaseMob
+        //{
+        //    get
+        //    {
+        //        if (_baseMob == null)
+        //        {
+        //            //Todo: Look into how Resources.Load() works
+        //            _baseMob = Resources.Load("Mob");
+        //        }
+        //        return _baseMob;
+        //    }
+        //}
+
+        // Use this for initialization
+        void Start () {
+            this.PlaceStartPoint();
+            this.PlaceEndPoint();
+            
+            //Todo: Spawn enemies at start point
+            this.SpawnMob();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	        //Todo: Check if any mobs are alive
+        }
+
+        #region Mob spawning
+
+        private void SpawnMob()
+        {
+            Instantiate(this.BaseMob, this._startingPointObject.transform.position, Quaternion.identity);
+        }
+
+        #endregion
+
+
+        #region point positioning and spawning
+
+        private void PlaceStartPoint()
+        {
+            Vector3 startPoint = new Vector3(-20f, 1f, 0f);
+            this._startingPointObject = ((GameObject)Instantiate(this.StartPointPrefab, startPoint, Quaternion.identity)).GetComponent<StartPosition>();
+        }
+
+        private void PlaceEndPoint()
+        {
+            Vector3 endPoint = new Vector3(20f, 1f, 0f);
+            this._endPointObject = ((GameObject)Instantiate(this.EndPointPrefab, endPoint, Quaternion.identity)).GetComponent<EndPosition>();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Mobs/BaseMob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Mobs
+{
+    public class BaseMob : MonoBehaviour
+    {
+
+        protected NavMeshAgent NavMeshAgent;
+        protected Rigidbody Rigidbody;
+
+        // Use this for initialization
+        void Start ()
+        {
+            this.NavMeshAgent = this.GetComponent<NavMeshAgent>();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	        this.NavMeshAgent.Move(new Vector3(20, 1, 0) * Time.deltaTime * 0.5f);
+            //this.NavMeshAgent.Move();
+            //Todo: Get the end position from the game manager
+        }
+    }
+}
