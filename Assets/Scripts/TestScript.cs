fb5e762cc8d5a147f16a20edcaa582aba9ce8aec
Arūnas Seniucas <Arnas Seniucas>
2016-04-21 22:05:10 +0300

    Edited git ignore. Added F# project

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using TDLibrary;
 public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    Debug.Log(new Class1().GetFive());
 	}
 	
 	// Update is called once per frame
---
+++ TDLibrary/Library1.fs
@@ -0,0 +1,12 @@
+﻿namespace TDLibrary
+
+open UnityEngine
+
+type public Class1() = 
+    member this.X = "F#"
+    member this.GetFive() = 5
+
+
+type public TowerHelper() =
+    static member public GetInRangeTurrets(startingPosition : Vector3, range : float32, gameObjects : GameObject[]) = 
+        5
