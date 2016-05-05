b81fe58846b5770fded9243dfa0eeaefaae69cd9
Arunas <arunas.seniucas@outlook.com>
2016-05-05 18:36:08 +0300

    Started working on the buildings

---
+++ Assets/Scripts/Buildings/ABuilding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Buildings
+{
+    public class ABuilding : MonoBehaviour, IBuilding
+    {
+        protected BoxCollider RangeCollider;
+
+        void Start()
+        {
+            this.RangeCollider = GetComponent<BoxCollider>(); //Todo: Arunas figure out how to use multiple components of the same type.
+        }
+    }
+}
